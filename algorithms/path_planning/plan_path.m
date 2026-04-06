7426613cc859f9f623b0b332b04261471703abc2
Vít Hromek <vitasalesautomation@gmail.com>
2026-04-06 22:13:36 +0200

    Week 3 hotovo bez reportu

--- algorithms/path_planning/plan_path.m
+++ algorithms/path_planning/plan_path.m
@@ -1,19 +1,46 @@
 function [path] = plan_path(read_only_vars, public_vars)
 %PLAN_PATH Summary of this function goes here
+%tri testosvaci trajektorie: primka, kruhovy oblouk, sinusovka
+%primka
+path_type = 3;
 
-planning_required = 1;
-
-if planning_required
-    
-    path = astar(read_only_vars, public_vars);
-    
-    path = smooth_path(path);
-    
+if path_type==1
+    x=linspace(2,18,30);
+    y=2*ones(size(x));
+    path=[x' y'];
+elseif path_type == 2
+    t=linspace(pi,pi/2,30);
+    R=4;
+    cx=6;
+    cy=6;
+    x=cx + R*cos(t);
+    y=cy+R*sin(t);
+    path=[x' y'];
+elseif path_type==3
+    x=linspace(2,18, 50);
+    y=7+2*sin(0.6*x);
+    path=[x' y'];
 else
-    
-    path = public_vars.path;
-    
+    path=[2 2;4 2;6 2];
 end
+
+
+
+% 
+% 
+% planning_required = 1;
+% 
+% if planning_required
+%     
+%     path = astar(read_only_vars, public_vars);
+%     
+%     path = smooth_path(path);
+%     
+% else
+%     
+%     path = public_vars.path;
+%     
+% end
 
 end
 
--- algorithms/student_workspace.m
+++ algorithms/student_workspace.m
@@ -1,6 +1,22 @@
 function [public_vars] = student_workspace(read_only_vars,public_vars)
 %STUDENT_WORKSPACE Summary of this function goes here
 
+% %%tvorba mapy:
+% 9,9 %cil robota
+% 0,0,20,20 %hranice mapy [x_min, y_min, x_max, y_max]
+% 0, 0, 20, 0 % stěny ve formátu: [x1, y1, x2, y2]
+% 20, 0, 20, 20
+% 20, 20, 0, 20
+% 0, 20, 0, 0
+% 1, 1, 5, 1
+% inf %všechno nad inf jsou stěny, všechno pod jsou oblasti
+% 0,0,20,0,20,20,0,20 %asi gnss denied oblast [x1,y1,x2,y2,x3,y3,x4,y4]
+% %%protože do texťáku nejdou přidávat poznámky, tak si to odložím sem.
+
+
+
+
+
 % 8. Perform initialization procedure
 if (read_only_vars.counter == 1)
           
@@ -16,7 +32,7 @@ public_vars.particles = update_particle_filter(read_only_vars, public_vars);
 [public_vars.mu, public_vars.sigma] = update_kalman_filter(read_only_vars, public_vars);
 
 % 11. Estimate current robot position
-public_vars.estimated_pose = estimate_pose(public_vars); % (x,y,theta)
+public_vars.estimated_pose = estimate_pose(read_only_vars,public_vars); % (x,y,theta)
 
 % 12. Path planning
 public_vars.path = plan_path(read_only_vars, public_vars);
