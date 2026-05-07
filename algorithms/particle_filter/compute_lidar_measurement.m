a8a0f4d9a450e5e254575eadf21cf7ec73d4cc26
Vít Hromek <vitasalesautomation@gmail.com>
2026-05-07 19:55:47 +0200

    Copy particle filter from week 4

--- algorithms/particle_filter/compute_lidar_measurement.m
+++ algorithms/particle_filter/compute_lidar_measurement.m
@@ -3,5 +3,25 @@ function [measurement] = compute_lidar_measurement(map, pose, lidar_config)
 
 measurement = zeros(1, length(lidar_config));
 
+ray_origin = pose(1:2);
+theta = pose(3);
+
+for i= 1:length(lidar_config)
+    direction = theta + lidar_config(i);
+
+    intersections = ray_cast(ray_origin, map.walls, direction);
+
+    if isempty(intersections)
+        measurement(i)=inf;
+    else
+        dx = intersections(:,1) - ray_origin(1);
+        dy = intersections(:,2) - ray_origin(2);
+        d = sqrt(dx.^2 + dy.^2);
+        measurement(i) = min(d);
+    end
+end
+
+
+
 end
 
--- algorithms/particle_filter/weight_particles.m
+++ algorithms/particle_filter/weight_particles.m
@@ -2,7 +2,33 @@ function [weights] = weight_particles(particle_measurements, lidar_distances)
 %WEIGHT_PARTICLES Summary of this function goes here
 
 N = size(particle_measurements, 1);
-weights = ones(N,1) / N;
+weights = zeros(N,1);
+sigma = 0.7; %zde si vytváříme proměnou pro porovnání lidaru
+
+
+for i = 1:N
+    pred=particle_measurements(i,:);
+    real=lidar_distances;
+    valid=isfinite(real) & isfinite(pred);
+
+    if sum(valid)<3
+        weights(i)=1e-12;
+    else
+        diff=pred(valid)-real(valid);
+        mse=mean(diff.^2);
+        if isnan(mse) || isinf(mse)
+            weights(i)=1e-12;
+        else
+            weights(i)=exp(-mse/(2*sigma^2));
+        end
+    end
+end
+
+
+weights(~isfinite(weights))=1e-12;
+weights=weights+1e-12;
+weights=weights/sum(weights);
+
 
 end
 
